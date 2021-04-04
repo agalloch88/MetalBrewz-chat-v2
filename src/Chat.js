0954518e8b05db0b376f44369a08b995b14853e9
Ryan Kirsch <agalloch88@gmail.com>
2021-04-04 12:13:16 -0400

    sidebar buildout and styling, channels, profile info

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chat.css';
+import ChatHeader from './ChatHeader';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h2>I am Chat</h2>
+            <ChatHeader />
+        </div>
+    )
+}
+
+export default Chat;
